2fbe74a8112ff8dfe46c29fb4c41e0ea6b4e9449
nyg4628 <nyg4628@gmail.com>
2017-09-21 13:56:11 +0900

    modified not found page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const route = require('./route/route.js');
+const route = require('./route');
 
 app.use(function (req, res, next) {
     console.log('RootTime:', Date.now());
@@ -8,6 +8,19 @@ app.use(function (req, res, next) {
 });
 
 app.use('/', route);
+
+// 404 처리 부분
+app.use((req, res, next) => {
+    res.status(404).send('Not found page');
+    next();
+});
+
+// 에러 처리 부분
+app.use((err, req, res, next) => {
+    console.error(err.stack); // 에러 메시지 표시
+    res.status(500).send('Server Error!!'); // 500 상태 표시 후 에러 메시지 전송
+    next();
+});
 
 app.listen(3000, function () {
     console.log("Port on 3000!");
